60f19a30787c3f77adcadcc8ca7d67c885864496
anzmatrimony <srinivas.a@massil.tech>
2017-03-24 19:32:57 +0530

    anz_profile_basicsearch
    added by
        Gnanendra Kumar

---
+++ anz_profile_basicsearch (1)/src (1)/main (1)/java (1)/anz_profile_basicsearch (1)/List (1).java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package anz_profile_basicsearch;
+
+/**
+ * @author acer
+ *
+ */
+public class List {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
